f14ceea6b3aa533ff7541db81757394da9034eb9
Hesam Kashefi <hkashefi7@outlook.com>
2024-07-21 09:03:52 +0330

    added template files

--- projects/ngx-duration-picker/src/lib/ngx-duration-picker.component.ts
+++ projects/ngx-duration-picker/src/lib/ngx-duration-picker.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'lib-NgxDurationPicker',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      ngx-duration-picker works!
-    </p>
-  `,
-  styles: ``
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './ngx-duration-picker.component.html',
+  styleUrls: ['./ngx-duration-picker.component.scss']
 })
 export class NgxDurationPickerComponent {
-
+  form = new FormGroup({
+    hours: new FormControl(0, [Validators.min(0)]),
+    minutes: new FormControl(0, [Validators.min(0)]),
+    seconds: new FormControl(0, [Validators.min(0)])
+  });
 }
